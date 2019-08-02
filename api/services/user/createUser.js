bf30bff691e8dd6daadf926b15a246c506806d74
ktan114 <ktaneight@gmail.com>
2019-08-02 18:25:33 -0400

    Updated the createUser function

--- api/services/user/createUser.js
+++ api/services/user/createUser.js
@@ -1,15 +1,17 @@
 const User = require('../../models/User');
+const createResult = require('../result/createResult');
 
 const createUser = async (email, result) => {
+  if (!email || !result) return null;
+  const resultId = await createResult(result);
   const user = {
     email,
-    result,
+    result: resultId,
   };
 
   const newUser = new User(user);
-  const response = await newUser.save();
-
-  return response ? 'Success' : 'Error';
+  const response = newUser.save();
+  return response;
 };
 
 module.exports = createUser;
