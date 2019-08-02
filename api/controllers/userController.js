c9278975757498d0f6b8ff2591578f6cdbc695eb
ktan114 <ktaneight@gmail.com>
2019-08-02 18:29:33 -0400

    Updated the userController + index for userService

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const createUser = require('../services/user/createUser');
+const userService = require('../services/user');
 
 router.post('/', async (req, res) => {
   const { email, result } = req.body;
 
-  const response = await createUser(email, result);
+  const response = await userService.createUser(email, result);
   response ? res.status(200).json(response) : res.status(500).json('Error');
 });
 
