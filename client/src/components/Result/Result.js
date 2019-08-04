b20d047372ded07d70e2603ab42f2e342d36de9c
ktan114 <ktaneight@gmail.com>
2019-08-04 18:38:34 -0400

    Added boilerplate text to the Result page

--- client/src/components/Result/Result.js
+++ client/src/components/Result/Result.js
@@ -5,8 +5,20 @@ const Result = props => {
   const { result } = props.location.state;
   return (
     <div>
-      <h1>HELLO RESULT PAGE</h1>
-      <h1>Your personality type is: {result}</h1>
+      <h1>Your Perspective</h1>
+      <h1>Your Perspective Type is: {result}</h1>
+      <p>
+        <span>Introversion (I)</span> <span>Extraversion (E)</span>
+      </p>
+      <p>
+        <span>Sensing (S)</span> <span>Intuition (I)</span>
+      </p>
+      <p>
+        <span>Thinking (T)</span> <span>Feeling (F)</span>
+      </p>
+      <p>
+        <span>Judging (J)</span> <span>Perceiving (P)</span>
+      </p>
     </div>
   );
 };
